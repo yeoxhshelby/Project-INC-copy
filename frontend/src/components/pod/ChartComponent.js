b13fc0a54054bc1aece93f49731da34c9ec8e3de
Adeeb <adityaadeeb04@gmail.com>
2022-06-20 00:19:10 +0800

    dynamically addind data

--- frontend/src/components/pod/ChartComponent.js
+++ frontend/src/components/pod/ChartComponent.js
@@ -1,25 +1,63 @@
+import React, { useRef } from "react";
+import * as d3 from "d3";
 
-
-import React, { useEffect, useRef } from "react";
-import drawChart from "./drawChart";
+const colors = ["#0CA85D", "#9F43CC", "#2B87E3", "#EBA10F"];
+const boxSize = 500;
 
 const DonutChart = ({ data }) => {
   const ref = useRef(null);
 
-  useEffect(() => {
-    if (ref.current) {
-      drawChart(ref.current, data);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ref]);
+  if (data) {
+    d3.select(ref.current).select("svg").remove(); // Remove the old svg
 
- 
-  
+    // Create new svg
+    const svg = d3
+      .select(ref.current)
+      .append("svg")
+      .attr("preserveAspectRatio", "xMidYMid meet")
+      .attr("height", "130%")
+      .attr("width", "130%")
+      .attr("viewBox", `0 0 ${boxSize} ${boxSize}`)
+      .append("g")
+      .attr("transform", `translate(${boxSize / 2}, ${boxSize / 2})`);
+
+    const arcGenerator = d3.arc().innerRadius(150).outerRadius(220);
+
+    const pieGenerator = d3.pie().value((d) => d.value);
+
+    const arcs = svg.selectAll().data(pieGenerator(data.value)).enter();
+
+    //recipe label
+    const recipe = svg
+      .append("g")
+      .attr("class", "bar-header")
+      .attr("transform", `translate(-80, 2)`)
+      .attr("font-weight", 900)
+      .style("font-size", "2.5em")
+      .append("text");
+
+    recipe.append("tspan").text(data.data[0].name);
+
+    recipe
+      .append("tspan")
+      .attr("x", 35)
+      .attr("dy", "1.8em")
+      .style("font-size", "0.6em")
+      .style("fill", "#555")
+      .attr("font-weight", 400)
+      .text("2 Hours");
+
+    arcs
+      .append("path")
+      .attr("d", arcGenerator)
+      .style("fill", (d, i) => colors[i % data.value.length]);
+  }
+
   return (
     <div className="container">
       <div className="graph" ref={ref} />
     </div>
   );
-};                                                                                                                                                                                                                                                
+};
 
 export default React.memo(DonutChart);
--- frontend/src/pages/ProductionOverview.js
+++ frontend/src/pages/ProductionOverview.js
@@ -1,104 +1,117 @@
+import React, { useEffect, useState } from "react";
 import FetchData from "../components/pod/fetchdata";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
 import "../styles/styles.css";
+import ChartComponent from "../components/pod/ChartComponent";
 
 const ProductionOverview = () => {
-  const data = [
-    { value: 25 },
-    { value: 35 }, 
-    { value: 45 },
-    { value: 15 }
-  ];
+  const [prodOverviewData, setProdOverviewData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/api/data/data2")
+      .then((res) => res.json())
+      .then((data) => {
+        // Below should be just setProdOverviewData(data), where data is your full backend data.
+        setProdOverviewData({
+          data: data.data,
+          value: [{ value: 25 }, { value: 35 }, { value: 45 }, { value: 15 }],
+        });
+      });
+  }, []);
 
   return (
     <div className="App">
       <div className="Row1">
-      <FetchData data={data} />
-      {/* Start of boxes */}
-      <div className="Boxes">
-      <div className="BoxRow1"> 
-          <Box
-          sx={{
-            width: 135,
-            height: 70,
-            backgroundColor: '#0CA85D',
-            '&:hover': {
-              backgroundColor: 'primary.main',
-              opacity: [0.9, 0.8, 0.7],
-            },
-            padding: 1.2,
-            fontSize: 12,
-            borderRadius: 1,
-            marginTop:15,
-            marginLeft: 40
-            }}>
-          <div className="BoxHeader">Weighing (1st Step)</div>
-          <div className="BoxSubHeader">25 minutes</div>
-          </Box>
-          <Box
-          sx={{
-            width: 135,
-            height: 70,
-            backgroundColor: '#9F43CC',
-            '&:hover': {
-              backgroundColor: 'primary.main',
-              opacity: [0.9, 0.8, 0.7],
-            },
-            padding: 1.2,
-            fontSize: 12,
-            borderRadius: 1,
-            marginLeft: 3,
-            marginTop: 15
-            }}>
-          <div className="BoxHeader">Mixing (2nd Step)</div>
-          <div className="BoxSubHeader">35 minutes</div>
-          </Box>
-      </div>
+        {/* <FetchData data={data} /> */}
+        <ChartComponent data={prodOverviewData} />
+        {/* Start of boxes */}
+        <div className="Boxes">
+          <div className="BoxRow1">
+            <Box
+              sx={{
+                width: 135,
+                height: 70,
+                backgroundColor: "#0CA85D",
+                "&:hover": {
+                  backgroundColor: "primary.main",
+                  opacity: [0.9, 0.8, 0.7],
+                },
+                padding: 1.2,
+                fontSize: 12,
+                borderRadius: 1,
+                marginTop: 15,
+                marginLeft: 40,
+              }}
+            >
+              <div className="BoxHeader">Weighing (1st Step)</div>
+              <div className="BoxSubHeader">25 minutes</div>
+            </Box>
+            <Box
+              sx={{
+                width: 135,
+                height: 70,
+                backgroundColor: "#9F43CC",
+                "&:hover": {
+                  backgroundColor: "primary.main",
+                  opacity: [0.9, 0.8, 0.7],
+                },
+                padding: 1.2,
+                fontSize: 12,
+                borderRadius: 1,
+                marginLeft: 3,
+                marginTop: 15,
+              }}
+            >
+              <div className="BoxHeader">Mixing (2nd Step)</div>
+              <div className="BoxSubHeader">35 minutes</div>
+            </Box>
+          </div>
 
-      <div className="BoxRow2">
-      <Box
-          sx={{
-            width: 135,
-            height: 70,
-            backgroundColor: '#EBA10F',
-            '&:hover': {
-              backgroundColor: 'primary.main',
-              opacity: [0.9, 0.8, 0.7],
-            },
-            padding: 1.2,
-            fontSize: 12,
-            borderRadius: 1,
-            marginTop: 3,
-            marginLeft: 40
-            }}>
-          <div className="BoxHeader">Baking (3rd Step)</div>
-          <div className="BoxSubHeader">15 minutes</div>
-          </Box>
-          <Box
-          sx={{
-            width: 135,
-            height: 70,
-            backgroundColor: '#2B87E3',
-            '&:hover': {
-              backgroundColor: 'primary.main',
-              opacity: [0.9, 0.8, 0.7],
-            },
-            padding: 1.2,
-            fontSize: 12,
-            borderRadius: 1,
-            marginLeft: 3,
-            marginTop: 3
-            }}>
-          <div className="BoxHeader">Packing (4th Step)</div>
-          <div className="BoxSubHeader">45 minutes</div>
-          </Box>
-      </div>
-      </div>
-      {/* End of boxes */}
+          <div className="BoxRow2">
+            <Box
+              sx={{
+                width: 135,
+                height: 70,
+                backgroundColor: "#EBA10F",
+                "&:hover": {
+                  backgroundColor: "primary.main",
+                  opacity: [0.9, 0.8, 0.7],
+                },
+                padding: 1.2,
+                fontSize: 12,
+                borderRadius: 1,
+                marginTop: 3,
+                marginLeft: 40,
+              }}
+            >
+              <div className="BoxHeader">Baking (3rd Step)</div>
+              <div className="BoxSubHeader">15 minutes</div>
+            </Box>
+            <Box
+              sx={{
+                width: 135,
+                height: 70,
+                backgroundColor: "#2B87E3",
+                "&:hover": {
+                  backgroundColor: "primary.main",
+                  opacity: [0.9, 0.8, 0.7],
+                },
+                padding: 1.2,
+                fontSize: 12,
+                borderRadius: 1,
+                marginLeft: 3,
+                marginTop: 3,
+              }}
+            >
+              <div className="BoxHeader">Packing (4th Step)</div>
+              <div className="BoxSubHeader">45 minutes</div>
+            </Box>
+          </div>
+        </div>
+        {/* End of boxes */}
       </div>
     </div>
-
   );
-}
+};
 
 export default ProductionOverview;
